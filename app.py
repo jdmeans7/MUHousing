74019c7f683c1659ace5f1334eeb775c7a7f2a06
Jason Means <means41@live.marshall.edu>
2018-04-18 21:12:52 -0400

    Reorganized app and query files

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 from pymysql import *
 import pymysql.cursors
 #import models
-#import query
+import query
 
 
 app = Flask(__name__)
@@ -14,25 +14,12 @@ app.config['SECRET_KEY'] = 'SecretSecret'
 #                             db='muhousing',
 #                             charset='utf8mb4',
 #                             cursorclass=pymysql.cursors.DictCursor)
-connection = pymysql.connect(host='sql9.freesqldatabase.com',
-                             user='sql9233446',
-                             password='crjxGGauSm',
-                             db='sql9233446',
-                             charset='utf8mb4',
-                             cursorclass=pymysql.cursors.DictCursor)
 
-try:
-    with connection.cursor() as cursor:
-        # Read a single record
-        sql = "SELECT `FirstName`, `LastName` FROM `Advisor` WHERE `email`=%s"
-        cursor.execute(sql, ('puc@marshall.edu',))
-        result = cursor.fetchone()
-finally:
-    connection.close()
+querydb = query.query()
 
 @app.route('/')
 def index():
-    return render_template('Index.html', result=result)
+    return render_template('Index.html', result=querydb.test('puc@marshall.edu'))
 
 if __name__ == '__main__':
     app.run()
--- query.py
+++ query.py
@@ -1,8 +1,21 @@
-import models as db
-from peewee import *
+import pymysql.cursors
+
+connection = pymysql.connect(host='sql9.freesqldatabase.com',
+                             user='sql9233446',
+                             password='crjxGGauSm',
+                             db='sql9233446',
+                             charset='utf8mb4',
+                             cursorclass=pymysql.cursors.DictCursor)
 
 class query(object):
-    def getStudent(self, bannerNumber):
-        s = db.Student.select(db.Student.FirstName).where(db.Student.bannerNumber == bannerNumber)
-        return s
+    def test(self, email):
+        try:
+            with connection.cursor() as cursor:
+                # Read a single record
+                sql = "SELECT `FirstName`, `LastName` FROM `Advisor` WHERE `email`=%s"
+                cursor.execute(sql, email)
+                result = cursor.fetchone()
+        finally:
+            connection.close()
+        return result
 
